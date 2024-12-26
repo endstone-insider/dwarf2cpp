cc9c8c8c5dbec6431523fbf7cc14ce6c80d114ac
Vincent <magicdroidx@gmail.com>
2024-12-26 18:25:21 +0000

    feat: merge info by calling `parse` multiple times on struct-like objects and typedefs

--- src/main.cpp
+++ src/main.cpp
@@ -22,64 +22,81 @@ std::unordered_map<std::string, dwarf2cpp::SourceFile> files;
 
 void parse_children(const llvm::DWARFDie &die, std::vector<std::string> &namespaces) // NOLINT(*-no-recursion)
 {
-    for (auto child : die.children()) {
-        child = child.resolveTypeUnitReference();
+    for (const auto &child : die.children()) {
         const auto tag = child.getTag();
-        std::string name;
-        if (auto *buffer = child.getShortName(); buffer) {
-            name = buffer;
-        }
 
         if (tag == llvm::dwarf::DW_TAG_namespace) {
+            std::string name;
+            if (auto *buffer = child.getShortName(); buffer) {
+                name = buffer;
+            }
             namespaces.push_back(name);
             parse_children(child, namespaces);
             namespaces.pop_back();
             continue;
         }
 
-        if (!child.find(llvm::dwarf::DW_AT_name) || !child.find(llvm::dwarf::DW_AT_decl_file) ||
-            !child.find(llvm::dwarf::DW_AT_decl_line)) {
+        auto child_type = child.resolveTypeUnitReference();
+        if (!child_type.find(llvm::dwarf::DW_AT_name) || !child_type.find(llvm::dwarf::DW_AT_decl_file) ||
+            !child_type.find(llvm::dwarf::DW_AT_decl_line)) {
             continue;
         }
 
-        auto decl_line = child.getDeclLine();
-        auto decl_file = child.getDeclFile(llvm::DILineInfoSpecifier::FileLineInfoKind::AbsoluteFilePath);
+        auto decl_line = child_type.getDeclLine();
+        auto decl_file = child_type.getDeclFile(llvm::DILineInfoSpecifier::FileLineInfoKind::AbsoluteFilePath);
+        std::replace(decl_file.begin(), decl_file.end(), '\\', '/');
+        decl_file = posixpath::normpath(decl_file);
 
-        std::unique_ptr<dwarf2cpp::Entry> entry;
-        switch (tag) {
-        case llvm::dwarf::DW_TAG_class_type: {
-            entry = std::make_unique<dwarf2cpp::StructLike>(dwarf2cpp::StructLike::Kind::Class, namespaces);
-            break;
-        }
-        case llvm::dwarf::DW_TAG_enumeration_type: {
-            entry = std::make_unique<dwarf2cpp::Enum>(namespaces);
-            break;
-        }
-        case llvm::dwarf::DW_TAG_structure_type: {
-            entry = std::make_unique<dwarf2cpp::StructLike>(dwarf2cpp::StructLike::Kind::Struct, namespaces);
-            break;
-        }
-        case llvm::dwarf::DW_TAG_typedef: {
-            entry = std::make_unique<dwarf2cpp::Typedef>(namespaces);
-            break;
-        }
-        case llvm::dwarf::DW_TAG_union_type: {
-            entry = std::make_unique<dwarf2cpp::StructLike>(dwarf2cpp::StructLike::Kind::Union, namespaces);
-            break;
-        }
-        case llvm::dwarf::DW_TAG_subprogram: {
-            entry = std::make_unique<dwarf2cpp::Function>(false, namespaces);
-            break;
-        }
-        default:
-            break;
-        }
-
-        if (entry) {
-            entry->parse(child);
-            std::replace(decl_file.begin(), decl_file.end(), '\\', '/');
-            decl_file = posixpath::normpath(decl_file);
+        if (tag == llvm::dwarf::DW_TAG_subprogram) {
+            auto entry = std::make_unique<dwarf2cpp::Function>(false, namespaces);
+            entry->parse(child_type);
             files[decl_file].add(decl_line, std::move(entry));
+            continue;
+        }
+
+        dwarf2cpp::Entry *existing_entry = nullptr;
+        if (auto it = files.find(decl_file); it != files.end()) {
+            existing_entry = files[decl_file].get(decl_line);
+        }
+
+        if (!existing_entry) {
+            std::unique_ptr<dwarf2cpp::Entry> entry;
+            switch (tag) {
+            case llvm::dwarf::DW_TAG_class_type: {
+                entry = std::make_unique<dwarf2cpp::StructLike>(dwarf2cpp::StructLike::Kind::Class, namespaces);
+                break;
+            }
+            case llvm::dwarf::DW_TAG_enumeration_type: {
+                entry = std::make_unique<dwarf2cpp::Enum>(namespaces);
+                break;
+            }
+            case llvm::dwarf::DW_TAG_structure_type: {
+                entry = std::make_unique<dwarf2cpp::StructLike>(dwarf2cpp::StructLike::Kind::Struct, namespaces);
+                break;
+            }
+            case llvm::dwarf::DW_TAG_typedef: {
+                entry = std::make_unique<dwarf2cpp::Typedef>(namespaces);
+                break;
+            }
+            case llvm::dwarf::DW_TAG_union_type: {
+                entry = std::make_unique<dwarf2cpp::StructLike>(dwarf2cpp::StructLike::Kind::Union, namespaces);
+                break;
+            }
+            default:
+                break;
+            }
+
+            if (entry) {
+                files[decl_file].add(decl_line, std::move(entry));
+                existing_entry = files[decl_file].get(decl_line);
+            }
+        }
+
+        if (existing_entry) {
+            existing_entry->parse(child_type);
+            if (child_type != child) {
+                existing_entry->parse(child);
+            }
         }
     }
 }
@@ -149,10 +166,6 @@ int main(int argc, char **argv)
 
         std::vector<std::string> namespaces;
         parse_children(cu_die, namespaces);
-
-        // if (i >= 10) {
-        //     break;
-        // }
     }
 
     spdlog::info("Build dir: {}", build_dir);
